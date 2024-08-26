5f78aff0e856b62d6629cae9216857c0ca2561be
TheVerseer <boukkameron@gmail.com>
2024-08-26 11:21:36 -0500

    4

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from modules.askforname import *
 from modules.clearscreen import *
 
 while True:
+	clearScreen()
+
 	inputString: str = "Select a Module:\n\n1: AskForName\n" + "\nModule: "
 	option: str = input(inputString)
-
+	
 	if option.isdigit():
 		if option == "1":
 			askForName()
