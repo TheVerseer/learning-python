d5a2879b92d2770a945c303e6523a3cc8068e5f9
TheVerseer <boukkameron@gmail.com>
2024-08-26 13:04:17 -0500

    32

--- library/askforname.py
+++ library/askforname.py
@@ -9,9 +9,11 @@ def askForName():
         print()
         if option.isdigit and option == "1":
             name = input("name: ")
+            clearScreen()
             print(f"└── added {name} \n")
             namelist.insert(1, name)
         elif option.isdigit and option == "2":
+            clearScreen()
             for name in namelist:
                 print("Hi,", name)
             namelist = []
