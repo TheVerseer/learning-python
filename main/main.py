45972565fa495ea5bd8ac72c14c2dba3410b9ca6
TheVerseer <boukkameron@gmail.com>
2024-08-26 11:52:52 -0500

    17

--- main/main.py
+++ main/main.py
@@ -1,16 +1,17 @@
-from library.askforname import *
-from library.clearscreen import *
+#rom library.askforname import *
+#from library.clearscreen import *
 
-from main.functions import *
+#from main.functions import *
 
 while True:
-	clearScreen()
+	#clearScreen()
 
 	inputString: str = "Select a Module:\n\n1: AskForName\n" + "\nModule: "
 	option: str = input(inputString)
 	
 	if option.isdigit():
 		if option == "1":
-			askForName()
+			print('what')
+			#askForName()
 
 	print("\nPress 'Enter' or 'Return' to go back \nPress 'Esc' to exit")
--- run.bat
+++ run.bat
@@ -1,12 +1,5 @@
 @echo off
 
-python -m pip --version
-
-IF %ERRORLEVEL% NEQ 0 (
-    echo Pip is not installed, installing pip...
-    python -m ensurepip --upgrade
-)
-
 python -m pip install -r requirements.txt
 
 cd main
