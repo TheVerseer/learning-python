4a653ca4ff94f7cf7c9156d18cf7a303f03bc34c
TheVerseer <boukkameron@gmail.com>
2024-08-26 11:49:49 -0500

    15

--- run.bat
+++ run.bat
@@ -1,5 +1,13 @@
 @echo off
-REM python -m pip --version
+
+python -m pip --version
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Pip is not installed, installing pip...
+    python -m ensurepip --upgrade
+)
+
 python -m pip install -r requirements.txt
+
 py main.main.py
 pause >nul
