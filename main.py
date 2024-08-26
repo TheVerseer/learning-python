1acd4c320069b65bd2077e94c7f0bd9905edefd8
TheVerseer <boukkameron@gmail.com>
2024-08-26 12:59:22 -0500

    30

--- main.py
+++ main.py
@@ -15,16 +15,5 @@ while True:
 	if option.isdigit():
 		if option == "1":
 			askForName()
-
-	print("\nPress 'Enter' or 'Return' to go back \nPress 'Esc' and 'Enter' to exit")
-
-	while True:
-		ch = msvcrt.getch()
-		if ch == b'\r' or ch == b'\n':
-			break
-		elif ch == b'\x1b':
-			exit = True
-			break
-
-	if exit:
-		break
+			
+	input("\nPress 'Enter' or 'Return' to go back")
