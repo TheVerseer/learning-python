68d4ee8ff283950dbd8f3f4f2454edab50985d94
TheVerseer <boukkameron@gmail.com>
2024-08-26 10:35:50 -0500

    qa

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ inputString: str = "Select a Module:\n\n1: AskForName\n" + "\nModule: "
 option: str = input(inputString)
 
 if option.isdigit():
-	if option == "1":
+	if option == "1": 123423142
 		askForName()
