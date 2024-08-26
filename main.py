dcc9c95669cdc5c4d942fdd8593c8e734ad2b528
TheVerseer <boukkameron@gmail.com>
2024-08-26 12:20:54 -0500

    24

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from math import fabs
 from library.askforname import *
 from library.clearscreen import *
 
 from utility.functions import *
 
+import keyboard
+
 while True:
+	exit: bool = False
 	clearScreen()
 
 	inputString: str = "Select a Module:\n\n1: AskForName\n" + "\nModule: "
@@ -14,3 +18,13 @@ while True:
 			askForName()
 
 	print("\nPress 'Enter' or 'Return' to go back \nPress 'Esc' to exit")
+	while True:
+		if keyboard.is_pressed('enter'):
+			exit = False
+			break
+		elif keyboard.is_pressed('esc'):
+			exit = True
+			break
+
+	if exit:
+		break
